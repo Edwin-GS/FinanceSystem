6e78517821f6ed2e2bdc9b3838ddf4aed0c4fdba
ErinsonBeltre <erinsonbeltre05@gmail.com>
2023-10-07 02:11:07 -0400

    CambiosLogin1.3

--- projects/applications/client/src/app/app.module.ts
+++ projects/applications/client/src/app/app.module.ts
@@ -5,11 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { SingInComponent } from './components/sing-in/sing-in.component';
-import { SingUpComponent } from './components/sing-up/sing-up.component';
+
 
 @NgModule({
-  declarations: [AppComponent, SingInComponent, SingUpComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- projects/libraries/helpers/src/lib/components/auth/login/login.component.ts
+++ projects/libraries/helpers/src/lib/components/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {HandlerService} from "../../../services/handler.service";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'lib-login',
@@ -7,44 +9,72 @@ import {HandlerService} from "../../../services/handler.service";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  /**
-   * Login with admin only by email
-   * **/
+ appName="Legoft";
+ logoUrl='assets/logo/Legoft-Logo-OK-01-HIGH.png';
+ dashboard='legoft-lab/client/:user/:user_id';
 
   @Input() isAdmin = false;
 
-  constructor(private hs: HandlerService) {
+  loginForm!: FormGroup;
+  isLoggedin: boolean = false;
+  showAlert: boolean = false; // Variable para mostrar/ocultar la alerta
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private hs: HandlerService
+  ) {
+   this.loginForm = this.formBuilder.group({
+    user: this.formBuilder.group({
+      email: ['',
+      [Validators.required,
+        Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) // Validación de correo electrónico)]
+      ]],
+      password: ['',
+      [Validators.required,
+        Validators.minLength(8)  //validacion contraseña
+      ]],
+    })
+   });
+
   }
 
   ngOnInit(): void {
 
-    this.login()
   }
 
-  login() {
-
-    localStorage.setItem('LEGOFT_SID_SITE', '')
-
-    let user = {userOrEmail: "andres25", password: "Andres25."}
-
-    this.hs.post(user, `users/login`)
-      .subscribe((resp) => {
-
-        if (resp['success'] === false) {
-
-          console.log('Error creating user')
-
-        } else {
-
-          console.log(resp)
-
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Limpiamos el token de sesión en el almacenamiento local
+      localStorage.setItem('LEGOFT_SID_SITE', '');
+      const user = this.loginForm.value.user;
+      const userId = this.loginForm.value.userId;
+      this.showAlert = false;
+  
+      // Realizamos una solicitud POST para iniciar sesión
+      this.hs.post(user, 'users/login')
+        .subscribe((resp) => {
+          if (resp['success'] === false) {
+            console.log('Error al iniciar sesión'); // Mensaje de error al iniciar sesión
+            this.showAlert = true;
+          } else {
+            console.log(resp, 'Esto es la respuesta');
+            this.isLoggedin = true;
+            const updatedDashboard = this.dashboard
+              .replace(':user', user)
+              .replace(':user_id', userId);
+            // Redirigimos al usuario a su panel de control después de iniciar sesión
+            this.router.navigate([updatedDashboard, { user, userId }]);
+          }
+        }, 
+        (err) => {
+          console.error('Error al iniciar sesión: ' + err); // Mensaje de error en la solicitud de inicio de sesión
+          //this.showAlert = true;
         }
-      }, (err) => {
-
-        console.error('Error creating user: ' + err);
-      }
-
-    )
+      );
+    }
   }
+
 }
+
+//let user = {userOrEmail: "andres25", password: "Andres25."}
