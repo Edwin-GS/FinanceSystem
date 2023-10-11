b14e14172511b88747e43c8b74005294a6f55fc2
Jcoding17 <juliofiguereo07@gmail.com>
2023-10-10 22:47:27 -0400

    login form funcionando

--- projects/libraries/helpers/src/lib/components/auth/auth.routes.ts
+++ projects/libraries/helpers/src/lib/components/auth/auth.routes.ts
@@ -1,40 +1,44 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NotFoundComponent} from "./not-found/not-found.component";
-import {LoginComponent} from "./login/login.component";
-import {SignupComponent} from "./signup/signup.component";
-import {VerifyEmailComponent} from "./verify-email/verify-email.component";
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
+import { isloggedGuard } from '../../guards/islogged.guard';
 
 const AuthRoutes: Routes = [
-
   {
     path: 'login',
     title: 'Finance System',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [isloggedGuard],
   },
   {
     path: 'signup',
     title: 'Finance System',
-    component: SignupComponent
+    component: SignupComponent,
+    canActivate: [isloggedGuard],
   },
   {
     path: 'not-found',
     title: 'Finance System',
-    component: NotFoundComponent
+    component: NotFoundComponent,
   },
   {
     path: 'finance-system',
     loadChildren: () =>
-      import('../../../../../../applications/client/src/app/components/dashboard/dashboard.module').then(m => m.DashboardModule)
+      import(
+        '../../../../../../applications/client/src/app/components/dashboard/dashboard.module'
+      ).then((m) => m.DashboardModule),
   },
   {
     path: 'verify-email/:confirm',
     title: 'Legoft',
-    component: VerifyEmailComponent
+    component: VerifyEmailComponent,
   },
   {
     path: '**',
-    redirectTo: '/not-found'
-  }
+    redirectTo: '/not-found',
+  },
 ];
 
 export const AUTH_ROUTES = RouterModule.forChild(AuthRoutes);
