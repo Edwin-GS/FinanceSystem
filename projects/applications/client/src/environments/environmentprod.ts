7c45fe821068476a1bade60c1b79a63a12574979
Pedro Acevedo <thepeterace@gmail.com>
2023-10-29 21:52:19 -0400

    Added Properties CRUD

--- projects/applications/client/src/environments/environmentprod.ts
+++ projects/applications/client/src/environments/environmentprod.ts
@@ -1,4 +1,5 @@
 export const environmentProd = {
-  LEGOFT_BACKEND_URL: 'https://legoft.net',
+  // LEGOFT_BACKEND_URL: 'https://legoft.net',
   LEGOFT_FRONTEND_URL: 'https://legoft.com',
+  LEGOFT_BACKEND_URL: 'http://legoft.es:30300',
 };
--- projects/libraries/helpers/src/lib/models/profession.doc.ts
+++ projects/libraries/helpers/src/lib/models/profession.doc.ts
@@ -2,7 +2,7 @@ import { IProfession } from "../interfaces/profession.itc";
 
 export class Profession implements IProfession {
     constructor(
-           public _id: '',
+           public _id: '' | undefined,
            public nombre: ''
         ){}
 }
